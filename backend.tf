5c3c332e8cb94525839910f6937b740517269742
anmutetech <124485136+anmutetech@users.noreply.github.com>
2023-05-08 21:46:35 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,8 +2,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "jenkins-bucket43982789"
+    bucket = "s3-backend-anmute"
     key    = "remote.tfstate"
     region = "us-east-1"
+    dynamodb_table = "kmsbackend"
   }
 }
